61786cc84e8b4d95bce78d9b0fc1670e1024821e
lfsc09 <lfsc09@gmail.com>
2024-07-25 18:14:10 -0300

    update: requesting users data from the gateway and updating the datapool

--- src/app/pages/restricted/users/users-list/users-list.component.ts
+++ src/app/pages/restricted/users/users-list/users-list.component.ts
@@ -1,22 +1,25 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngleLeft, faAngleRight, faAnglesLeft, faAnglesRight, faFilter, faPlus, faSquareCheck } from '@fortawesome/free-solid-svg-icons';
 import { KdsDatapoolService } from '../../../../components/shared/kds/kds-datapool/kds-datapool.service';
 import { KdsLoadingInfiniteComponent } from '../../../../components/shared/kds/kds-loading-infinite/kds-loading-infinite.component';
+import { UsersGatewayService } from '../../../../infra/gateways/users/users-gateway.service';
+import { DatapoolUser } from '../../../../infra/gateways/users/users-gateway.model';
 
 @Component({
 	selector: 'app-users-list',
 	standalone: true,
 	imports: [FontAwesomeModule, RouterLink, KdsLoadingInfiniteComponent],
-	providers: [KdsDatapoolService],
+	providers: [KdsDatapoolService, UsersGatewayService],
 	templateUrl: './users-list.component.html',
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
 	/**
 	 * SERVICES
 	 */
 	protected readonly kdsDatapoolService = inject(KdsDatapoolService);
+	private readonly usersGatewayService = inject(UsersGatewayService);
 
 	/**
 	 * SIGNALS
@@ -30,5 +33,12 @@ export class UsersListComponent {
 		faAnglesLeft: faAnglesLeft,
 		faAnglesRight: faAnglesRight,
 	});
-	protected usersDatapool = this.kdsDatapoolService.getDatapool();
+    protected loading = signal<boolean>(false);
+
+	async ngOnInit(): Promise<void> {
+		this.loading.set(true);
+		const response = await this.usersGatewayService.getDatapoolFake({ currPageIdx: 0, rowsPerPage: 5 });
+		this.loading.set(false);
+		if (response) this.kdsDatapoolService.newDataReceived<DatapoolUser>(response);
+	}
 }
