67958e6199fdf542968c798b29b94f2a80cd5971
lfsc09 <lfsc09@gmail.com>
2024-09-18 09:09:38 -0300

    update: locale configured for correct use in pipes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import localeBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeBr);
 
 export const appConfig: ApplicationConfig = {
-	providers: [provideRouter(routes)],
+	providers: [provideRouter(routes), { provide: LOCALE_ID, useValue: 'pt' }],
 };
