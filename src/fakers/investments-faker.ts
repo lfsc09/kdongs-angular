4bbe4f7151553b34d8542a6d4840d9f0de2ba2ad
lfsc09 <lfsc09@gmail.com>
2024-10-04 22:51:10 -0300

    update: Added abstract `Faker` class with default functions and properties

--- src/fakers/investments-faker.ts
+++ src/fakers/investments-faker.ts
@@ -1,18 +1,9 @@
 import { delay, Observable, of, throwError } from 'rxjs';
 import { GetInvestmentsPerformanceResponse } from '../app/infra/gateways/investments/investments-gateway.model';
 import { InvestmentsFakerData } from './investments-faker.data';
+import { Faker } from './_default-faker';
 
-export class InvestmentsFaker {
-	private static readonly defaultRequestTime: [number, number] = [500, 2500];
-	private static readonly requestFailProbability: number = 0.05;
-
-	private static randomVariables(): { failRequest: boolean; requestTime: number } {
-		return {
-			failRequest: Math.trunc(Math.random() * 100) <= this.requestFailProbability * 100,
-			requestTime: Math.floor(Math.random() * (this.defaultRequestTime[1] - this.defaultRequestTime[0] + 1)) + this.defaultRequestTime[0],
-		};
-	}
-
+export class InvestmentsFaker extends Faker {
 	static getInvestmentsPerformance_Query1(failRequest?: boolean): Observable<GetInvestmentsPerformanceResponse> {
 		const generateRandomness = this.randomVariables();
 		// Fail the Request
