c4364bd99fde2c49e8ee9f2d556e635430d0fcb6
lfsc09 <lfsc09@gmail.com>
2024-10-31 10:36:30 -0300

    feat: theme now saved on `localStorage`

--- src/app/infra/services/theme/theme-manager.service.ts
+++ src/app/infra/services/theme/theme-manager.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal } from '@angular/core';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
 	providedIn: 'root',
@@ -10,15 +11,23 @@ export class ThemeManagerService {
 	private _darkTheme = signal<boolean>(false);
 	darkTheme = this._darkTheme.asReadonly();
 
+	constructor() {
+		const themeRead = localStorage.getItem(`theme:${environment.host}`) === 'dark';
+		this._darkTheme.set(themeRead);
+	}
+
 	invert() {
 		this._darkTheme.update((previous) => !previous);
+		localStorage.setItem(`theme:${environment.host}`, this._darkTheme() ? 'dark' : 'light');
 	}
 
 	goDark() {
 		this._darkTheme.set(true);
+		localStorage.setItem(`theme:${environment.host}`, 'dark');
 	}
 
 	goLight() {
 		this._darkTheme.set(false);
+		localStorage.setItem(`theme:${environment.host}`, 'light');
 	}
 }
