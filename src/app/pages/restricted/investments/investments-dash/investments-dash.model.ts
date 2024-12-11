9350617444f7cbc46a3a7ef4e0f5e47325e218a3
lfsc09 <lfsc09@gmail.com>
2024-12-11 10:37:26 -0300

    fix: missing `Currency` typing

--- src/app/pages/restricted/investments/investments-dash/investments-dash.model.ts
+++ src/app/pages/restricted/investments/investments-dash/investments-dash.model.ts
@@ -5,7 +5,7 @@ export type Section = 'performance' | 'balance_history' | 'assets';
 export type SelectableCurrency = 'Wallet' | Currency;
 
 export type SelectableWalletsMap_Key = string;
-export type SelectableWalletsMap_Value = { input_balance_percentage_of: string; profit_percentage_of: string; currency: string } | null;
+export type SelectableWalletsMap_Value = { input_balance_percentage_of: string; profit_percentage_of: string; currency: Currency } | null;
 export type SelectableWallets = Map<SelectableWalletsMap_Key, SelectableWalletsMap_Value>;
 
 export type UserPreferences = {
